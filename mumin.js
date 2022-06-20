cde0e81ebeabad7adbe275fe6452a91a2e8a6d23
abdul-mumin001 <abdulmuminbar@gmail.com>
2022-06-21 01:49:45 +0530

    2ndcommit

--- mumin.js
+++ mumin.js
@@ -1,11 +1,44 @@
 //Q. sum of square of digit
 
-let num=12
-let sum=0;
-while(num!=0){
-    let rem=num%10;
-    sum=sum+rem*2;
-    num=Math.floor(num/10);
+// let num=12
+// let sum=0;
+// while(num!=0){
+//     let rem=num%10;
+//     sum=sum+rem*2;
+//     num=Math.floor(num/10);
     
-} 
-console.log(sum)
+// } 
+// console.log(sum)
+
+// const num=parseInt(process.argv[2]);
+// const n=num.length;
+// let sum=0;
+// let x= num;
+// while(x>0){
+//     let rem=x%10;
+//     sum+=rem**n;
+//     x=parseInt(x/10)
+// }
+// if(sum==num){
+//     console.log("is an armstrong")
+// }else{
+//     console.log("is not an armstrong")
+// }
+
+//6Q. Take a number and return all even digit of numbers
+
+// function even_digits(num) {
+//     var x = 0;
+//     while (num) {
+//       x += num % 2 === 0;
+//       num = Math.floor(num / 10);
+//     }
+//     return x;
+//   }
+  
+//   console.log(even_digits(7860));
+ 
+
+
+
+
